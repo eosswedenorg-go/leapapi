5e4bf98e018d4bdf9ba275e946283a1a7579f132
Henrik Hautakoski <henrik@eossweden.org>
2022-01-03 18:17:23 +0100

    Convert hard tabs to soft.

--- functions.go
+++ functions.go
@@ -2,80 +2,80 @@
 package eosapi;
 
 import (
-	"strings"
-	"time"
-	"net/url"
-	"io/ioutil"
-	"github.com/imroc/req"
-	jsontime "github.com/liamylian/jsontime/v2/v2"
+    "strings"
+    "time"
+    "net/url"
+    "io/ioutil"
+    "github.com/imroc/req"
+    jsontime "github.com/liamylian/jsontime/v2/v2"
 )
 
 var json = jsontime.ConfigWithCustomTimeFormat
 
 func init() {
 
-	// EOS Api does not specify timezone in timestamps (they are always UTC tho).
-	jsontime.SetDefaultTimeFormat("2006-01-02T15:04:05", time.UTC)
+    // EOS Api does not specify timezone in timestamps (they are always UTC tho).
+    jsontime.SetDefaultTimeFormat("2006-01-02T15:04:05", time.UTC)
 }
 
 func send(p ReqParams, method string, path string) (*req.Resp, error) {
 
-	host := p.Host
-	if len(host) < 1 {
-		u, err := url.Parse(p.Url)
-		if err != nil {
-	        return nil, err
-	    }
-		host = strings.Split(u.Host, ":")[0]
-	}
+    host := p.Host
+    if len(host) < 1 {
+        u, err := url.Parse(p.Url)
+        if err != nil {
+            return nil, err
+        }
+        host = strings.Split(u.Host, ":")[0]
+    }
 
-	// Go's net.http (that `req` uses) sends the port in the host header.
-	// nodeos api does not like that, so we need to provide our
-	// own Host header with just the host.
-	headers := req.Header{
-		"Host": host,
-	}
+    // Go's net.http (that `req` uses) sends the port in the host header.
+    // nodeos api does not like that, so we need to provide our
+    // own Host header with just the host.
+    headers := req.Header{
+        "Host": host,
+    }
 
-	r := req.New()
-	return r.Do(method, p.Url + path, headers)
+    r := req.New()
+    return r.Do(method, p.Url + path, headers)
 }
 
 //  GetInfo - Fetches "/v1/chain/get_info" from API
 // ---------------------------------------------------------
 func GetInfo(params ReqParams) (Info, error) {
 
-	var info Info
+    var info Info
 
-	r, err := send(params, "GET", "/v1/chain/get_info")
-	if err == nil {
-		resp := r.Response()
-		body, _ := ioutil.ReadAll(resp.Body)
+    r, err := send(params, "GET", "/v1/chain/get_info")
+    if err == nil {
+        resp := r.Response()
+        body, _ := ioutil.ReadAll(resp.Body)
 
-		// Set HTTPStatusCode
-		info.HTTPStatusCode = resp.StatusCode
+        // Set HTTPStatusCode
+        info.HTTPStatusCode = resp.StatusCode
 
-		// Parse json
-		err = json.Unmarshal(body, &info)
-	}
-	return info, err
+        // Parse json
+        err = json.Unmarshal(body, &info)
+    }
+    return info, err
 }
 
 //  Health - Fetches "/v2/health" from API
 // ---------------------------------------------------------
 func GetHealth(params ReqParams) (Health, error) {
 
-	var health Health;
+    var health Health;
 
-	r, err := send(params, "GET", "/v2/health")
-	if err == nil {
-		resp := r.Response()
-		body, _ := ioutil.ReadAll(resp.Body)
+    r, err := send(params, "GET", "/v2/health")
+    if err == nil {
+        resp := r.Response()
+        body, _ := ioutil.ReadAll(resp.Body)
 
         // Set HTTPStatusCode
         health.HTTPStatusCode = resp.StatusCode
 
-		// Parse json
-		err = json.Unmarshal(body, &health)
-	}
-	return health, err
+        // Parse json
+        err = json.Unmarshal(body, &health)
+    }
+    return health, err
 }
